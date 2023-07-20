0ad71b9caf2d6a4a860cb0784dead3e2471d6bac
woidead <abikbest05@gmail.com>
2023-07-20 15:14:25 +0600

    2007

---
+++ main-page/app.js
@@ -0,0 +1,9 @@
+function toggleDropdown() {
+    let dropdownContent = document.getElementById("myDropdown");
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  }
+  
